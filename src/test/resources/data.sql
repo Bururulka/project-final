7059c35c8c3b70e303682b8c2cb38e48e9d3f077
Bururulka <vl.tarasov01@gmail.com>
2025-09-03 20:10:15 +0300

    first commit

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -8,25 +8,19 @@ from PROFILE;
 
 delete
 from ACTIVITY;
-alter
-sequence ACTIVITY_ID_SEQ restart with 1;
+ALTER TABLE ACTIVITY ALTER COLUMN ID RESTART WITH 1;
 delete
 from TASK;
-alter
-sequence TASK_ID_SEQ restart with 1;
+ALTER TABLE TASK ALTER COLUMN ID RESTART WITH 1;
 delete
 from SPRINT;
-alter
-sequence SPRINT_ID_SEQ restart with 1;
+ALTER TABLE SPRINT ALTER COLUMN ID RESTART WITH 1;
 delete
 from PROJECT;
-alter
-sequence PROJECT_ID_SEQ restart with 1;
-
+ALTER TABLE PROJECT ALTER COLUMN ID RESTART WITH 1;
 delete
 from USERS;
-alter
-sequence USERS_ID_SEQ restart with 1;
+ALTER TABLE USERS ALTER COLUMN ID RESTART WITH 1;
 
 insert into USERS (EMAIL, PASSWORD, FIRST_NAME, LAST_NAME, DISPLAY_NAME)
 values ('user@gmail.com', '{noop}password', 'userFirstName', 'userLastName', 'userDisplayName'),
